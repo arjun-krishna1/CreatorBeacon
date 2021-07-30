39374885290d00e016f86f6998120d18cf445049
Joshua Dietrich <Jdietrich0603@gmail.com>
2021-07-30 17:26:29 -0400

    qr fixes

--- qrcodefunctions/__init__.py
+++ qrcodefunctions/__init__.py
@@ -5,11 +5,12 @@ from pathlib import Path
 
 static_path = os.path.join(Path(__file__).parents[1], 'static')
 
-def make_qr(data):
+
+def make_qr(data, name):
     code = make(data)
-    name = str(time()) + ".png"
-    code.save("static/" + name)
+    name = str(name) + ".png"
+    code.save(os.path.join(static_path, name))
     return name
 
 def make_website_link_qr(id):
-    return make_qr("http://127.0.0.1:8000/" + id)
+    return make_qr("http://127.0.0.1:8000/" + str(id), id)

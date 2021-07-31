4747163468c657bad0b73c324bdabd6c4f26bc08
arjun-krishna1 <arjunkrishna1306@gmail.com>
2021-07-31 16:24:18 -0400

    add cockroach db

---
+++ arjuns_secrets.py
@@ -0,0 +1,7 @@
+DB_INFO = {
+    "user": "arjun_krishna",
+    "password": "6EVKKWaBqMGbak",
+    "globalhost": "free-tier.gcp-us-central1.cockroachlabs.cloud",
+    "name": "shabby-bull-2624.db",
+    "sslrootcert": "/home/blueuser/.postgresql/root.crt",
+}
